df2555a0f96dbea72e0635c392322918137e3fe6
rafael <rrrr0ffff@gmail.com>
2024-02-09 23:59:59 -0300

    added basic animation

--- star-shaped ring/ring.js
+++ star-shaped ring/ring.js
@@ -2,14 +2,20 @@ class Ring {
   constructor(r, num) {
     this.r = r;
     this.num = num;
+
+    this.speed = 0;
   }
 
   display() {
     for (let i = 0; i < this.num; i++) {
       let angle = (360 / this.num) * i;
-      let x = this.r * cos(angle);
-      let y = this.r * sin(angle);
+      let x = this.r * cos(angle + this.speed);
+      let y = this.r * sin(angle + this.speed);
       ellipse(x, y, 10, 10);
     }
+  }
+
+  move() {
+    this.speed += 1;
   }
 }
