3e94eed052611dd26bb7b14efa4ae4e4fc364f62
rafael <rrrr0ffff@gmail.com>
2024-02-09 23:38:58 -0300

    rendering first border circle

--- star.js
+++ star.js
@@ -1,9 +1,16 @@
+let r = 150;
+let angle = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  let x = r * cos(angle);
+  let y = r * sin(angle);
   translate(width / 2, height / 2);
-  ellipse(0, 0, 300, 300);
+  ellipse(0, 0, r * 2, r * 2);
+  ellipse(x, y, 10, 10);
 }
