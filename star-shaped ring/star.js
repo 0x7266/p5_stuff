1a9df9317845089a42998da599a30374c7974f55
rafael <rrrr0ffff@gmail.com>
2024-02-09 23:57:15 -0300

    created instance of ring

--- star-shaped ring/star.js
+++ star-shaped ring/star.js
@@ -1,20 +1,14 @@
-let r = 150;
-let angle = 0;
-let num = 10;
-
+let r;
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
+
+  r = new Ring(150, 40);
 }
 
 function draw() {
   background(0);
   translate(width / 2, height / 2);
 
-  for (let i = 0; i < num; i++) {
-    let angle = (360 / num) * i;
-    let x = r * cos(angle);
-    let y = r * sin(angle);
-    ellipse(x, y, 10, 10);
-  }
+  r.display();
 }
