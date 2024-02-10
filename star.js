b1452183c4deb6f67e96bef9cda64ab83824120f
rafael <rrrr0ffff@gmail.com>
2024-02-09 23:42:43 -0300

    rendering all circles

--- star.js
+++ star.js
@@ -1,16 +1,20 @@
 let r = 150;
 let angle = 0;
+let num = 10;
 
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
-
-  let x = r * cos(angle);
-  let y = r * sin(angle);
   translate(width / 2, height / 2);
-  ellipse(0, 0, r * 2, r * 2);
-  ellipse(x, y, 10, 10);
+
+  for (let i = 0; i < num; i++) {
+    let angle = (360 / num) * i;
+    let x = r * cos(angle);
+    let y = r * sin(angle);
+    ellipse(x, y, 10, 10);
+  }
 }
