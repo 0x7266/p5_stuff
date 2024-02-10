1e56a2ccdc44addd1eb2eb5b06ff2909eea95fcd
rafael <rrrr0ffff@gmail.com>
2024-02-10 00:05:34 -0300

    added more rings

--- star-shaped ring/star.js
+++ star-shaped ring/star.js
@@ -1,8 +1,12 @@
-let r;
+let rings = [];
+let n = 9;
+
 function setup() {
   createCanvas(400, 400);
   angleMode(DEGREES);
-
+  for (let i = 0; i < n; i++) {
+    rings[i] = new Ring(150 - i * 10, 30);
+  }
   r = new Ring(150, 40);
 }
 
@@ -10,6 +14,8 @@ function draw() {
   background(0);
   translate(width / 2, height / 2);
 
-  r.display();
-  r.move();
+  for (let i = 0; i < n; i++) {
+    rings[i].display();
+    rings[i].move();
+  }
 }
