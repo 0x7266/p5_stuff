19ba7eab814c5c8d3aa6b1f57893896c18a1c197
rafael <rrrr0ffff@gmail.com>
2024-02-13 00:13:52 -0300

    added first mouse interactiveness

--- interactive grid/index.js
+++ interactive grid/index.js
@@ -8,16 +8,21 @@ function setup() {
   rectMode(CENTER);
   cols = width / spacing;
   rows = height / spacing;
-  for (let i = 0; i < cols; i++) {
-    size[i] = [];
-    for (let j = 0; j < rows; j++) {
-      size[i][j] = 10;
-    }
-  }
 }
 
 function draw() {
   background(220);
+  for (let i = 0; i < cols; i++) {
+    size[i] = [];
+    for (let j = 0; j < rows; j++) {
+      size[i][j] = dist(
+        mouseX,
+        mouseY,
+        spacing / 2 + i * spacing,
+        spacing / 2 + j * spacing,
+      );
+    }
+  }
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       rect(
