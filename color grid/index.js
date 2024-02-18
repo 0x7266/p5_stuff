f35d89881ddd95f3c0c43aba8cc684760a9e1179
rafael <rrrr0ffff@gmail.com>
2024-02-17 23:39:52 -0300

    added colors

--- color grid/index.js
+++ color grid/index.js
@@ -1,17 +1,25 @@
 let cols;
 let rows;
 let size = 20;
+let c = [];
 
 function setup() {
   createCanvas(400, 400);
   cols = width / size;
   rows = height / size;
+  for (let i = 0; i < cols; i++) {
+    c[i] = [];
+    for (let j = 0; j < rows; j++) {
+      c[i][j] = color(random(256), random(256), random(256));
+    }
+  }
 }
 
 function draw() {
   background(220);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
+      fill(c[i][j]);
       rect(i * size, j * size, size, size);
     }
   }
